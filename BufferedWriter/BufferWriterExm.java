5df65d42bf745e276c19ffda51e15e982ed29785
Deepschoudhary <156118658+Deepschoudhary@users.noreply.github.com>
2024-04-01 12:49:19 +0530

    new code add

--- BufferedWriter/BufferWriterExm.java
+++ BufferedWriter/BufferWriterExm.java
@@ -1,10 +1,20 @@
 package BufferedWriter;
 import java.io.*;
 
+//public class BufferWriterExm {
+//    public static void main(String[] args) throws IOException{
+//        BufferedWriter bf = new BufferedWriter(new FileWriter ("D:\\Java_practice\\Resources\\dilip.txt", true));
+//        bf.write(" Computer");
+//        bf.close();
+//    }
+//}
 public class BufferWriterExm {
-    public static void main(String[] args) throws IOException{
-        BufferedWriter bf = new BufferedWriter(new FileWriter ("D:\\Java_practice\\Resources\\dilip.txt", true));
-        bf.write(" Computer");
+    public static void main(String[] args) throws IOException {
+        FileWriter fw = new FileWriter("D:\\Java_practice\\Resources\\ram.txt", true);
+        BufferedWriter bf = new BufferedWriter(fw);
+        bf.write("NEW");
         bf.close();
+
     }
+
 }
